37eeb57ae8243203ba6bd3a847251fa1acc99dee
Javier Solis <javsolis@mit.edu>
2022-06-20 14:13:06 -0500

    Improvements to scripts

--- scripts/deploy_commands.sh
+++ scripts/deploy_commands.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd ~/project-team-js;
-git fetch && git reset origin/main --hard;
 python -m venv python3-virtualenv;
 source python3-virtualenv/bin/activate;
 pip install -r requirements.txt;
--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-chmod +x deploy_commands.sh;
+cd ~/project-team-js;
+
+git fetch && git reset origin/main --hard;
+chmod +x ~/project-team-js/scripts/deploy_commands.sh;
 tmux new-session -d -s  portfolio-site 'bash deploy_commands.sh';
 tmux detach -s portfolio-site;
