4cfeead1ae526fc68f52c307f11f962e8d521388
Javier Solis <javsolis@mit.edu>
2022-06-28 22:29:29 -0500

    Fixed bug in redeploy script

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -5,6 +5,7 @@ cd ~/project-team-js;
 git fetch && git reset origin/main --hard;
 chmod +x ~/project-team-js/scripts/deploy_commands.sh;
 
-./project-team-js/scripts/deploy_commands.sh
+systemctl disable myportfolio
+bash ~/project-team-js/scripts/deploy_commands.sh
 systemctl daemon-reload
 systemctl restart myportfolio
