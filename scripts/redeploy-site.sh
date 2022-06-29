415baf649934ec88e860a96945536ecb60d52aab
Javier Solis <javsolis@mit.edu>
2022-06-28 22:12:16 -0500

    Modified the redeploy script to reflect the use of a service

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -4,6 +4,7 @@ cd ~/project-team-js;
 
 git fetch && git reset origin/main --hard;
 chmod +x ~/project-team-js/scripts/deploy_commands.sh;
-tmux kill-session -t portfolio-site;
-tmux new-session -d -s portfolio-site;
-tmux send-keys -t portfolio-site "~/project-team-js/scripts/deploy_commands.sh" Enter;
+
+./project-team-js/scripts/deploy_commands.sh
+systemctl daemon-reload
+systemctl restart myportfolio
