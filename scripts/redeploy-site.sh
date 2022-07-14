8b12ab13d7b009ec5150ee782ccbb97eb0fecb18
Javier Solis <javsolis@mit.edu>
2022-07-14 15:57:28 -0500

    Modified the redeploy script to use Docker

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -4,10 +4,5 @@ cd ~/project-team-js;
 
 git fetch && git reset origin/main --hard;
 
-python -m venv python3-virtualenv;
-source python3-virtualenv/bin/activate;
-pip install -r requirements.txt;
-
-systemctl stop myportfolio
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
